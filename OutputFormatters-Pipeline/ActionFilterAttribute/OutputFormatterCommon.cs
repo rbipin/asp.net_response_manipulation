df8eb4386ddaa6deb1d0da786530c8cb6bc315b2
Bipin Radhakrishnan <hi@bipinr.com>
2020-05-26 07:43:51 -0400

    solution 1 check in
    output manipulating the output formatter

---
+++ OutputFormatters-Pipeline/ActionFilterAttribute/OutputFormatterCommon.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+
+namespace OutputFormatter.API
+{
+    public sealed class OutputFormatterCommon
+    {
+
+        /// <summary>
+        /// Clear the existing output formatter
+        /// </summary>
+        /// <param name="context">action context</param>
+        /// <param name="outputFormatter">Output formatter to add</param>
+        public void AddOutputFormatter(ActionExecutingContext context, IOutputFormatter outputFormatter)
+        {
+            var options = context
+        .HttpContext
+        .RequestServices
+        .GetService(serviceType: typeof(IOptions<MvcOptions>)) as IOptions<MvcOptions>;
+            var mvcOptions = options.Value;
+
+            mvcOptions.OutputFormatters.Add(outputFormatter);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="outputFormatters"></param>
+        public void ClearOutputFormatters(ActionExecutingContext context, List<IOutputFormatter> outputFormatters)
+        {
+
+            var options = context
+         .HttpContext
+         .RequestServices
+         .GetService(serviceType: typeof(IOptions<MvcOptions>)) as IOptions<MvcOptions>;
+            var mvcOptions = options.Value;
+
+            foreach (var outputFormatter in outputFormatters)
+            {
+                mvcOptions.OutputFormatters.RemoveType(outputFormatter.GetType());
+            }
+        }
+    }
+}
--- OutputFormatters-Pipeline/Startup.cs
+++ OutputFormatters-Pipeline/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace manual_serialize_in_actionfilter
+namespace OutputFormatter.API
 {
     public class Startup
     {
@@ -25,6 +19,7 @@ namespace manual_serialize_in_actionfilter
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddMvc().AddXmlSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
